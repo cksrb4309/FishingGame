f77fd48f259c81e807d758c2f523ee48d54566c6
cksrb4309 <rlacksrb0856@gmail.com>
2025-03-30 00:16:24 +0900

    스태미나 게이지 추가

---
+++ Assets/02_Scripts/PlayerUI/PlayerUIController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUIController : MonoBehaviour
+{
+    [Header("스태미나 UI")]
+    [SerializeField] CanvasGroup staminaCanvasGroup;
+    [SerializeField] Image staminaFillImage;
+    [SerializeField] float staminaHideTime = 1f;
+    [SerializeField] float staminaFadeSpeed = 1f;
+
+    float staminaViewTime = 10f;
+    public void Start()
+    {
+        staminaCanvasGroup.alpha = 0f;
+
+        StartCoroutine(StaminaCoroutine());
+    }
+    IEnumerator StaminaCoroutine()
+    {
+        while (true)
+        {
+            if (staminaViewTime < staminaHideTime)
+
+                staminaCanvasGroup.alpha += Time.deltaTime * staminaFadeSpeed;
+            
+            else staminaCanvasGroup.alpha -= Time.deltaTime * staminaFadeSpeed;
+
+            staminaViewTime += Time.deltaTime;
+
+            yield return null;
+        }
+    }
+    public void SetStaminaAmount(float amount)
+    {
+        staminaFillImage.fillAmount = amount;
+
+        staminaViewTime = 0f;
+    }
+}
